5c9088c6bed55970b03ffce69e6a3d1e66359d80
richard le <68008106+chongwongus@users.noreply.github.com>
2021-12-08 19:33:26 -0800

    fixing merge

--- src/controllers/Player.java
+++ src/controllers/Player.java
@@ -9,9 +9,6 @@ import java.io.Serializable;
  */
 public class Player implements Serializable{
 	
-  private static final long serialVersionUID = 1L;
-  public class Player implements Serializable {
-	
 	private static final long serialVersionUID = 1L;
 	
 	private int myPlayerX;
